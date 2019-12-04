e1129a12944ac136d42d8d0e7b8ff95a255f4a62
huajianzhang <278554189@qq.com>
2019-12-04 17:02:41 +0800

    修改模板

--- ZhangBlogIdea/blog/views.py
+++ ZhangBlogIdea/blog/views.py
@@ -18,7 +18,7 @@ class CommonViewMixin:
 
 class IndexView(CommonViewMixin, ListView):
     queryset = Post.latest_posts()
-    paginate_by = 3
+    paginate_by = 6
     context_object_name = 'post_list'
     template_name = 'blog/list.html'
 
